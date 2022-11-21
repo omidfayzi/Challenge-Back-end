d5f16da477e7208972e8f38ed953e6cb70087fde
omid <99048099@mydavinci.nl>
2022-11-21 11:49:31 +0100

    startpagina aangemaakt

--- display.php
+++ display.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Startpagina</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="stylesheet.css" rel="stylesheet">
   </head>
   <body>
 
   <div class="contaier">
-    <button class="btn btn-primary my-5">
+    <button class="btn btn-primary ">
     <a href="toDo.php" class="text-light text-decoration-none">Een ToDo toevoegen</a>
     </button>
 
@@ -19,7 +20,7 @@
       <th scope="col">#</th>
       <th scope="col">Naam</th>
       <th scope="col">Beschrijving</th>
-      <th scope="col">Tijdsduur</th>
+      <th scope="col">Tijdsduur in minuten</th>
       <th scope="col">Status</th>
       <th scope="col">Action</th>
     </tr>
@@ -42,7 +43,14 @@
         <td scope="row">'.$row["beschrijving"].'</td>
         <td scope="row">'.$row["tijdsduur"].'</td>
         <td scope="row">'.$row["status"].'</td>
-        <td scope="row"><button class="btn btn-warning"><a href="update.php" class="text-light text-decoration-none">wijzigen</a></button> &nbsp; <button class="btn btn-danger"><a href="delete.php" class="text-light text-decoration-none">verwijderen</a></button></td>
+        <td scope="row">
+          <button class="btn btn-warning">
+            <a href="update.php?updateid='.$row["Id"].'" class="text-light text-decoration-none">wijzigen</a>
+          </button> &nbsp; 
+          <button class="btn btn-danger">
+            <a href="delete.php?deleteid='.$row["Id"].'" class="text-light text-decoration-none">verwijderen</a>
+          </button>
+        </td>
       </tr>';
        }; 
     }
---
+++ script.js
@@ -0,0 +1,12 @@
+var statusText = document.getElementById('status');
+var statusMark = document.getElementById('statusMark');
+
+statusText.style.visibility = "hidden";
+
+
+statusMark.addEventListener('change', function () {
+    statusText.value = "voldaan";
+
+    console.log(statusText.value)
+})
+
